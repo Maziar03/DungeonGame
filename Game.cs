6d1e676fd2ca0862cc72232c7078b7fb023206f9
Maziar03 <126330922+Maziar03@users.noreply.github.com>
2025-03-11 12:35:54 +0000

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -46,7 +46,6 @@ namespace DungeonExplorer
             { "Hallway", new Room("A long, damp hallway stretches ahead. You can hear a faint sound in the distance—something’s up.","Treasure Room") },
             { "Treasure Room", new Room("A shiny treasure room stacked with gold—but there’s a monster keeping watch!", null, true) }
         };
-
             currentRoom = rooms["Entrance"];
             Console.WriteLine($"\nWelcome, {player.Name}! You enter the dungeon!!!");
         }
